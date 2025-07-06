8765daffce65afd327a9c77f77b7795e1a4ffe97
nixxoq <nixxoq@outlook.com>
2025-07-06 23:18:21 +0300

    feat(styles): add AccentColor enumerator

---
+++ include/rebuildTUI/styles.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace tui_extras {
+    enum class AccentColor {
+        CYAN,
+        BLUE,
+        GREEN,
+        RED,
+        YELLOW,
+        MAGENTA,
+        WHITE,
+        BRIGHT_CYAN,
+        BRIGHT_BLUE,
+        BRIGHT_GREEN,
+        BRIGHT_RED,
+        BRIGHT_YELLOW,
+        BRIGHT_MAGENTA,
+        BRIGHT_WHITE
+    };
+} // namespace tui_extras
