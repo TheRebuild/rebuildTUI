5101b72f1910a93bd8210e7584f2735e5455e4a9
nixxoq <nixxoq@outlook.com>
2025-07-21 22:19:11 +0300

    feat(styles): set correct integers for AccentColor enum

--- include/rebuildTUI/styles.hpp
+++ include/rebuildTUI/styles.hpp
@@ -13,20 +13,21 @@ namespace tui_extras {
     enum class BorderStyle { ROUNDED, DOUBLE, SHARP, ASCII };
 
     enum class AccentColor {
-        CYAN,
-        BLUE,
-        GREEN,
-        RED,
-        YELLOW,
-        MAGENTA,
-        WHITE,
-        BRIGHT_CYAN,
-        BRIGHT_BLUE,
-        BRIGHT_GREEN,
-        BRIGHT_RED,
-        BRIGHT_YELLOW,
-        BRIGHT_MAGENTA,
-        BRIGHT_WHITE
+        RESET = 0,
+        RED = 31,
+        GREEN = 32,
+        YELLOW = 33,
+        BLUE = 34,
+        MAGENTA = 35,
+        CYAN = 36,
+        WHITE = 37,
+        BRIGHT_RED = 91,
+        BRIGHT_GREEN = 92,
+        BRIGHT_YELLOW = 93,
+        BRIGHT_BLUE = 94,
+        BRIGHT_MAGENTA = 95,
+        BRIGHT_CYAN = 96,
+        BRIGHT_WHITE = 97
     };
 
     class GradientPreset {
